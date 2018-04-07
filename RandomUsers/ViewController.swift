0bf6479eb92dc463a1d5f6d2f813a2734262debb
Brian Weissberg <brianweissberg@gmail.com>
2018-04-07 11:19:15 -0600

    First commit to master

---
+++ RandomUsers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomUsers
+//
+//  Created by Brian Weissberg on 4/7/18.
+//  Copyright © 2018 Brian Weissberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
